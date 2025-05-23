7cca18f9ecc230dd8aea769e942205ec4913b750
LFJ RedMi <cnwjfx001@gmail.com>
2025-05-23 13:55:36 +1000

    redmi

---
+++ others/3000-3100/3068. 最大节点价值之和.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 32098 on 25-5-23.
+//
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+constexpr int MOD = 1'000'000'007;
+
+class Solution {
+public:
+  long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+
+  }
+};
