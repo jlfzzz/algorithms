2b1951bb990dbf0d5c5b05cc93aa0bbb65a9a306
sof1azzz <cnwjfx001@gmail.com>
2025-05-23 10:40:12 +1000

    Legion

---
+++ others/3200-3300/3290. 最高乘法得分.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 123 on 25-5-23.
+//
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+constexpr int MOD = 1'000'000'007;
+
+class Solution {
+public:
+  long long maxScore(vector<int>& a, vector<int>& b) {
+
+  }
+};
--- others/3300-3400/3362. 零数组变换 III.cpp
+++ others/3300-3400/3362. 零数组变换 III.cpp
@@ -13,6 +13,42 @@ public:
       return a[0] < b[0];
     });
 
+    int n = nums.size();
+    int m = queries.size();
+    int j = 0;
+    priority_queue<int> pq;
+    vector<int> diff(n + 2, 0);
+    int prefix_sum = 0;
+    for (int i = 0; i < n; i++) {
+      int x = nums[i];
+      prefix_sum += diff[i];
+      while (j < m && queries[j][0] <= i) {
+        pq.push(queries[j][1]);
+        j++;
+      }
+
+      while (!pq.empty() && prefix_sum < x && pq.top() >= i) {
+        prefix_sum++;
+        diff[pq.top() + 1]--;
+        pq.pop();
+      }
+
+      if (x > 0) {
+        return -1;
+      }
+    }
+
+    return pq.size();
+  }
+};
+
+class Solution2 {
+public:
+  int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+    ranges::sort(queries, [&](const auto& a, const auto& b) {
+      return a[0] < b[0];
+    });
+
     int n = nums.size(), j = 0, sum_d = 0;
     vector<int> diff(n + 1, 0);
     priority_queue<int> pq;
