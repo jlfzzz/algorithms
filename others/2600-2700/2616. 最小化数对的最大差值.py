a93867cdf7d73682ba2b8c96ec74ac2e157a9be2
LFJ RedMi <cnwjfx001@gmail.com>
2025-06-13 12:45:01 +1000

    redmi

---
+++ others/2600-2700/2616. 最小化数对的最大差值.py
@@ -0,0 +1,35 @@
+import all_algos
+from typing import List
+
+
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        nums.sort()
+        ans = 100000000
+
+        lo = 1
+        hi = nums[-1] - nums[0] + 1
+        n = len(nums)
+
+        def check(limit) -> bool:
+            cnt = 0
+            i = 0
+            while i < n - 1:
+                if nums[i + 1] - nums[i] <= limit:
+                    cnt += 1
+                    i += 2  # 跳过下一对
+                else:
+                    i += 1
+
+            return cnt >= p
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if check(mid):
+                ans = mid
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return ans
