f641be96f985280c230f7b98a42cc9d8747a9f21
sof1azzz <cnwjfx001@gmail.com>
2025-10-04 11:13:39 +1000

    10.4

---
+++ C_Compressed_Bracket_Sequence.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define i128 __int128_t
+#define int ll
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define For(i, n) for (int(i) = 0; (i) < (n); (i) += 1)
+constexpr int MOD = int(1e9 + 7);
+constexpr int MOD2 = int(998244353);
+constexpr long long inf = 0x3f3f3f3f3f3f3f3f / 2;
+
+namespace io {
+    void debug() { cerr << "\n"; }
+
+    template<typename T, typename... Args>
+    void debug(const string &s, T x, Args... args) {
+        cerr << s << " = " << x;
+        if (sizeof...(args) > 0)
+            cerr << ", ";
+        debug(args...);
+    }
+
+    template<typename... Args>
+    void prt(const Args &...args) {
+        ((cout << args << " "), ...);
+        cout << "\n";
+    }
+
+    template<typename T>
+    void prt_vec(const vector<T> &v) {
+        for (size_t i = 0; i < v.size(); i++) {
+            if (i)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void prt_vec(const vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            if (i > start_index)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename End, typename... Args>
+    void prt_end(const End &end, const Args &...args) {
+        ((cout << args << " "), ...);
+        cout << end;
+    }
+
+    template<typename... Args>
+    void prt_endl(const Args &...args) {
+        ((cout << args << " "), ...);
+        cout << endl;
+    }
+
+    template<typename T>
+    void rd(T &x) {
+        cin >> x;
+    }
+
+    template<typename T, typename... Args>
+    void rd(T &x, Args &...args) {
+        cin >> x;
+        rd(args...);
+    }
+
+    template<typename A, typename B>
+    void rd(pair<A, B> &p) {
+        cin >> p.first >> p.second;
+    }
+
+    template<typename T>
+    void rd_vec(vector<T> &v) {
+        for (auto &x: v) {
+            rd(x);
+        }
+    }
+
+    template<typename T>
+    void rd_vec(vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            rd(v[i]);
+        }
+    }
+} // namespace io
+
+using namespace io;
+
+int Multitest = 0;
+
+void init() {}
+
+void solve() {
+    int n;
+    rd(n);
+
+    vector<int> a(n + 1);
+    rd_vec(a, 1);
+
+    int ans = 0;
+    vector<int> diff(n + 1);
+    for (int i = 2; i <= n; i += 2) {
+        int pre = a[i - 1];
+        int cur = a[i];
+
+
+        if (cur > pre) {
+            ans *= 2;
+            ans += pre;
+            int have = cur - pre;
+
+            for (int j = i - 2; j >= 1; j -= 2) {
+                if (diff[j] > 0) {
+                    continue;
+                }
+
+                if (have + diff[j] > 0) {
+                    ans += -diff[j];
+                    have += diff[j];
+                } else {
+                    ans += have;
+                    break;
+                }
+            }
+        } else if (cur == pre) {
+            ans *= 2;
+            ans += cur;
+        } else {
+            ans += pre;
+        }
+        diff[i] = diff[i - 2] + cur - pre;
+    }
+
+    prt(ans);
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    if (Multitest) {
+        rd(T);
+    }
+    while (T--)
+        solve();
+    return 0;
+}
---
+++ codeforces/1500-1600/1556/D_Take_a_Guess.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define i128 __int128_t
+#define int ll
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define For(i, n) for (int(i) = 0; (i) < (n); (i) += 1)
+constexpr int MOD = int(1e9 + 7);
+constexpr int MOD2 = int(998244353);
+constexpr long long inf = 0x3f3f3f3f3f3f3f3f / 2;
+
+namespace io {
+    void debug() { cerr << "\n"; }
+
+    template<typename T, typename... Args>
+    void debug(const string &s, T x, Args... args) {
+        cerr << s << " = " << x;
+        if (sizeof...(args) > 0)
+            cerr << ", ";
+        debug(args...);
+    }
+
+    template<typename... Args>
+    void prt(const Args &...args) {
+        ((cout << args << " "), ...);
+        cout << "\n";
+    }
+
+    template<typename T>
+    void prt_vec(const vector<T> &v) {
+        for (size_t i = 0; i < v.size(); i++) {
+            if (i)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void prt_vec(const vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            if (i > start_index)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename End, typename... Args>
+    void prt_end(const End &end, const Args &...args) {
+        ((cout << args << " "), ...);
+        cout << end;
+    }
+
+    template<typename... Args>
+    void prt_endl(const Args &...args) {
+        ((cout << args << " "), ...);
+        cout << endl;
+    }
+
+    template<typename T>
+    void rd(T &x) {
+        cin >> x;
+    }
+
+    template<typename T, typename... Args>
+    void rd(T &x, Args &...args) {
+        cin >> x;
+        rd(args...);
+    }
+
+    template<typename A, typename B>
+    void rd(pair<A, B> &p) {
+        cin >> p.first >> p.second;
+    }
+
+    template<typename T>
+    void rd_vec(vector<T> &v) {
+        for (auto &x: v) {
+            rd(x);
+        }
+    }
+
+    template<typename T>
+    void rd_vec(vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            rd(v[i]);
+        }
+    }
+} // namespace io
+
+using namespace io;
+
+int Multitest = 0;
+
+void init() {}
+
+void solve() {
+    int n, k;
+    rd(n, k);
+
+    auto ask = [&](const string& s, int i, int j) -> int {
+        prt_endl(s, i, j);
+        int t;
+        rd(t);
+        return t;
+    };
+
+    int s12 = ask("and", 1, 2) + ask("or", 1, 2);
+    int s23 = ask("and", 2, 3) + ask("or", 2, 3);
+    int s13 = ask("and", 1, 3) + ask("or", 1, 3);
+
+    int a1 = (s12 + s13 - s23) / 2;
+    int a2 = s12 - a1;
+    int a3 = s13 - a1;
+
+    vector<int> temp{a1, a2, a3};
+
+    for (int i = 4; i <= n; i++) {
+        int s1i = ask("and", 1, i) + ask("or", 1, i);
+        int ai = s1i - a1;
+        temp.push_back(ai);
+    }
+
+    ranges::sort(temp);
+    cout << "finish" << " " << temp[k - 1] << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    if (Multitest) {
+        rd(T);
+    }
+    while (T--)
+        solve();
+    return 0;
+}
