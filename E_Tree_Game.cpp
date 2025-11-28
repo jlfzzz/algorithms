338121543cd4d3efd9186c7c3624ff4a8db6b21e
LFJ RedMi <cnwjfx001@gmail.com>
2025-11-28 22:00:39 +1100

    11.28

---
+++ E_Tree_Game.cpp
@@ -0,0 +1,228 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define i128 __int128_t
+#define db long double
+#define pb emplace_back
+#define pf emplace_front
+#define all(x) (x).begin(), (x).end()
+#define all2(x, i) (x).begin() + (i), (x).end()
+using pii = pair<ll, ll>;
+#define ull unsigned long long
+#define vi vector<int>
+#define vp vector<pii>
+#define vl vector<long long>
+#define vvi vector<vector<int>>
+#define vvp vector<vector<pii>>
+#define vvl vector<vector<long long>>
+#define D(i, j, k) for (int(i) = (j); (i) >= (k); (i)--)
+#define SZ(a) ((int) (a).size())
+#define prq priority_queue
+#define fi first
+#define se second
+constexpr int MOD2 = int(1e9 + 7);
+constexpr int MOD = int(998244353);
+constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+constexpr int inf = 0x3f3f3f3f;
+#define F(i, j, k) for (int(i) = (j); (i) <= (k); (i)++)
+
+namespace utils {
+    template<typename A, typename B>
+    ostream &operator<<(ostream &os, const pair<A, B> &p) {
+        return os << '(' << p.first << ", " << p.second << ')';
+    }
+
+    template<typename Tuple, size_t... Is>
+    void print_tuple(ostream &os, const Tuple &t, index_sequence<Is...>) {
+        ((os << (Is == 0 ? "" : ", ") << get<Is>(t)), ...);
+    }
+
+    template<typename... Args>
+    ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+        os << '(';
+        print_tuple(os, t, index_sequence_for<Args...>{});
+        return os << ')';
+    }
+
+    template<typename T, typename = decltype(begin(declval<T>())), typename = enable_if_t<!is_same_v<T, string>>>
+    ostream &operator<<(ostream &os, const T &v) {
+        os << '{';
+        bool first = true;
+        for (auto &x: v) {
+            if (!first)
+                os << ", ";
+            first = false;
+            os << x;
+        }
+        return os << '}';
+    }
+
+    void debug_out() { cerr << endl; }
+
+    template<typename Head, typename... Tail>
+    void debug_out(Head H, Tail... T) {
+        cerr << H;
+        if (sizeof...(T))
+            cerr << " ";
+        debug_out(T...);
+    }
+
+    template<typename T>
+    void prt(const T &x) {
+        cout << x << '\n';
+    }
+
+    template<typename T, typename... Args>
+    void prt(const T &first, const Args &...rest) {
+        cout << first;
+        ((cout << ' ' << rest), ...);
+        cout << '\n';
+    }
+
+    template<typename T>
+    void prv(const vector<T> &v) {
+        for (size_t i = 0; i < v.size(); i++) {
+            if (i)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void prv(const vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            if (i > start_index)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void rd(T &x) {
+        cin >> x;
+    }
+
+    template<typename T, typename... Args>
+    void rd(T &x, Args &...args) {
+        cin >> x;
+        rd(args...);
+    }
+
+    template<typename A, typename B>
+    void rd(pair<A, B> &p) {
+        cin >> p.first >> p.second;
+    }
+
+    template<typename T>
+    void rv(vector<T> &v) {
+        for (auto &x: v) {
+            rd(x);
+        }
+    }
+
+    template<typename T>
+    void rv(vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            rd(v[i]);
+        }
+    }
+} // namespace utils
+
+#ifdef WOAIHUTAO
+#define dbg(...) cerr << "[L" << __LINE__ << " " << __func__ << " | " << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+#else
+#define dbg(...) ((void) 0)
+#endif
+
+using namespace utils;
+
+constexpr int N = 1e6 + 5;
+
+int Multitest = 0;
+
+void init() {}
+
+void solve() {
+    int n;
+    rd(n);
+
+    vvi g(n + 1);
+    vvi has(n + 1, vi(n + 1, 0));
+    F(i, 1, n - 1) {
+        int u, v;
+        rd(u, v);
+        g[u].pb(v);
+        g[v].pb(u);
+        has[u][v] = 1;
+        has[v][u] = 1;
+    }
+
+    set<pii> edges;
+    F(i, 1, n) {
+        vi dis(n + 1, -1);
+        auto bfs = [&]() {
+            queue<int> q;
+            q.push(i);
+            dis[i] = 0;
+
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (int v: g[u]) {
+                    if (dis[v] == -1) {
+                        dis[v] = dis[u] + 1;
+                        q.push(v);
+                    }
+                }
+            }
+        };
+        bfs();
+
+        F(j, 1, n) {
+            if (i != j && (dis[j] & 1) && !has[i][j]) {
+                auto [mn, mx] = minmax(i, j);
+                edges.insert({mn, mx});
+            }
+        }
+    }
+
+    int sz = SZ(edges);
+    if (sz & 1) {
+        cout << "First" << endl;
+    } else {
+        cout << "Second" << endl;
+    }
+
+    bool my_turn = (sz & 1);
+    while (true) {
+        if (my_turn) {
+            auto it = edges.begin();
+            cout << it->first << " " << it->second << endl;
+            edges.erase(it);
+            my_turn = false;
+        } else {
+            int a, b;
+            rd(a, b);
+            if (a == -1 && b == -1) {
+                return;
+            }
+            edges.erase({a, b});
+            my_turn = true;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    if (Multitest) {
+        rd(T);
+    }
+    while (T--) {
+        solve();
+    }
+}
---
+++ F_ABCBA.cpp
@@ -0,0 +1,265 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define i128 __int128_t
+#define db long double
+#define pb emplace_back
+#define pf emplace_front
+#define all(x) (x).begin(), (x).end()
+#define all2(x, i) (x).begin() + (i), (x).end()
+using pii = pair<ll, ll>;
+#define ull unsigned long long
+#define vi vector<int>
+#define vp vector<pii>
+#define vl vector<long long>
+#define vvi vector<vector<int>>
+#define vvp vector<vector<pii>>
+#define vvl vector<vector<long long>>
+#define D(i, j, k) for (int(i) = (j); (i) >= (k); (i)--)
+#define SZ(a) ((int) (a).size())
+#define prq priority_queue
+#define fi first
+#define se second
+constexpr int MOD2 = int(1e9 + 7);
+constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+constexpr int inf = 0x3f3f3f3f;
+#define F(i, j, k) for (int(i) = (j); (i) <= (k); (i)++)
+
+namespace utils {
+    template<typename A, typename B>
+    ostream &operator<<(ostream &os, const pair<A, B> &p) {
+        return os << '(' << p.first << ", " << p.second << ')';
+    }
+
+    template<typename Tuple, size_t... Is>
+    void print_tuple(ostream &os, const Tuple &t, index_sequence<Is...>) {
+        ((os << (Is == 0 ? "" : ", ") << get<Is>(t)), ...);
+    }
+
+    template<typename... Args>
+    ostream &operator<<(ostream &os, const tuple<Args...> &t) {
+        os << '(';
+        print_tuple(os, t, index_sequence_for<Args...>{});
+        return os << ')';
+    }
+
+    template<typename T, typename = decltype(begin(declval<T>())), typename = enable_if_t<!is_same_v<T, string>>>
+    ostream &operator<<(ostream &os, const T &v) {
+        os << '{';
+        bool first = true;
+        for (auto &x: v) {
+            if (!first)
+                os << ", ";
+            first = false;
+            os << x;
+        }
+        return os << '}';
+    }
+
+    void debug_out() { cerr << endl; }
+
+    template<typename Head, typename... Tail>
+    void debug_out(Head H, Tail... T) {
+        cerr << H;
+        if (sizeof...(T))
+            cerr << " ";
+        debug_out(T...);
+    }
+
+    template<typename T>
+    void prt(const T &x) {
+        cout << x << '\n';
+    }
+
+    template<typename T, typename... Args>
+    void prt(const T &first, const Args &...rest) {
+        cout << first;
+        ((cout << ' ' << rest), ...);
+        cout << '\n';
+    }
+
+    template<typename T>
+    void prv(const vector<T> &v) {
+        for (size_t i = 0; i < v.size(); i++) {
+            if (i)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void prv(const vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            if (i > start_index)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void rd(T &x) {
+        cin >> x;
+    }
+
+    template<typename T, typename... Args>
+    void rd(T &x, Args &...args) {
+        cin >> x;
+        rd(args...);
+    }
+
+    template<typename A, typename B>
+    void rd(pair<A, B> &p) {
+        cin >> p.first >> p.second;
+    }
+
+    template<typename T>
+    void rv(vector<T> &v) {
+        for (auto &x: v) {
+            rd(x);
+        }
+    }
+
+    template<typename T>
+    void rv(vector<T> &v, int start_index) {
+        for (int i = start_index; i < (int) v.size(); i++) {
+            rd(v[i]);
+        }
+    }
+} // namespace utils
+
+#ifdef WOAIHUTAO
+#define dbg(...) cerr << "[L" << __LINE__ << " " << __func__ << " | " << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+#else
+#define dbg(...) ((void) 0)
+#endif
+
+using namespace utils;
+
+mt19937 rng(random_device{}());
+
+int rnd(long long x, long long y) { return uniform_int_distribution<int>(x, y)(rng); }
+
+long long MOD = 1e18 + rnd(0, 1e9);
+int BASE = 233 + rnd(0, 1e3);
+
+struct HashSeq {
+    int n;
+    vector<__int128> P, H, HR;
+
+    HashSeq(string s) {
+        n = s.size();
+        P.resize(n + 1);
+        P[0] = 1;
+        for (int i = 1; i <= n; i++)
+            P[i] = P[i - 1] * BASE % MOD;
+
+        H.resize(n + 1);
+        H[0] = 0;
+        for (int i = 1; i <= n; i++)
+            H[i] = (H[i - 1] * BASE + (s[i - 1] ^ 7)) % MOD;
+
+        HR.resize(n + 1);
+        HR[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            HR[i] = (HR[i - 1] * BASE + (s[n - i] ^ 7)) % MOD;
+        }
+    }
+
+    long long query(int l, int r) const {
+        if (l > r)
+            return 0;
+        __int128 res = (H[r] - (H[l - 1] * P[r - l + 1]) % (__int128) MOD);
+        long long ans = (long long) ((res + (__int128) MOD) % (__int128) MOD);
+        return ans;
+    }
+
+    long long query_rev_on_reversed(int l, int r) const {
+        if (l > r)
+            return 0;
+        __int128 res = (HR[r] - (HR[l - 1] * P[r - l + 1]) % (__int128) MOD);
+        return (long long) ((res + (__int128) MOD) % (__int128) MOD);
+    }
+
+    // auto is_pal = [&](int l, int r) { return hs.query(l, r) == hs.query_rev_sub(l, r); };
+    long long query_rev_sub(int l, int r) const {
+        if (l > r)
+            return 0;
+        int L = n - r + 1;
+        int R = n - l + 1;
+        __int128 res = (HR[R] - (HR[L - 1] * P[R - L + 1]) % (__int128) MOD);
+        return (long long) ((res + (__int128) MOD) % (__int128) MOD);
+    }
+};
+
+constexpr int N = 1e6 + 5;
+
+int Multitest = 0;
+
+void init() {}
+
+void solve() {
+    string s;
+    rd(s);
+
+    HashSeq hs(s);
+    auto is_pal = [&](int l, int r) { return hs.query(l, r) == hs.query_rev_sub(l, r); };
+
+    int n = SZ(s);
+
+    if (n == 1) {
+        prt(s);
+        return;
+    }
+
+    string ans = s;
+    // if (n & 1) {
+    //     auto temp = s;
+    //     ranges::reverse(temp);
+    //     temp = temp.substr(1);
+    //     ans += temp;
+    // } else {
+    //     auto temp = s;
+    //     ranges::reverse(temp);
+    //     ans += temp;
+    // }
+
+    int last = -1;
+    D(i, n, 1) {
+        if (is_pal(i, n)) {
+            last = i;
+        }
+    }
+
+    // dbg(ans, last);
+
+    if (last != -1) {
+        auto temp = s;
+        int len = n - last + 1;
+        ranges::reverse(temp);
+        // int t = (len + 1) / 2;
+
+        // dbg(temp);
+        temp = temp.substr(len);
+        // dbg(temp);
+
+        ans = s;
+        ans += temp;
+    }
+
+    prt(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    if (Multitest) {
+        rd(T);
+    }
+    while (T--) {
+        solve();
+    }
+}
