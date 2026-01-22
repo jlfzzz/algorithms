c75f63d27ada54e099297f673733a9f7d136aea0
sof1azzz <cnwjfx001@gmail.com>
2026-01-22 23:19:57 +0800

    1.22

---
+++ AtCoder/abc/300-400/359/E_Water_Tank.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define i128 __int128_t
+#define db long double
+#define pb emplace_back
+#define pf emplace_front
+#define pob pop_back
+#define ep emplace
+#define ins insert
+#define all(x) (x).begin(), (x).end()
+#define all2(x, i) (x).begin() + (i), (x).end()
+using pii = pair<ll, ll>;
+#define ull unsigned long long
+#define us unsigned
+#define vi vector<int>
+#define vp vector<pii>
+#define vl vector<long long>
+#define vvi vector<vector<int>>
+#define vvp vector<vector<pii>>
+#define vvl vector<vector<long long>>
+#define D(i, j, k) for (int(i) = (j); (i) >= (k); (i)--)
+#define SZ(a) ((int) (a).size())
+#define prq priority_queue
+#define fi first
+#define se second
+constexpr int MOD2 = int(1e9 + 7);
+constexpr int MOD = int(998244353);
+constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+constexpr int inf = 0x3f3f3f3f;
+#define F(i, j, k) for (int(i) = (j); (i) <= (k); (i)++)
+
+namespace utils {
+    template<typename A, typename B>
+    ostream &operator<<(ostream &os, const pair<A, B> &p) {
+        return os << '(' << p.first << ", " << p.second << ')';
+    }
+    // ... (省略部分未修改的模板代码以节省空间) ...
+    template<typename T>
+    void prv(const vector<T> &v) {
+        for (size_t i = 0; i < v.size(); i++) {
+            if (i)
+                cout << " ";
+            cout << v[i];
+        }
+        cout << "\n";
+    }
+
+    template<typename T>
+    void rd(T &x) {
+        cin >> x;
+    }
+
+    template<typename T>
+    void rv(vector<T> &v) {
+        for (auto &x: v) {
+            rd(x);
+        }
+    }
+} // namespace utils
+using namespace utils;
+
+
+int Multitest = 0;
+
+void init() {}
+
+void solve() {
+    int N;
+    rd(N);
+    vl H(N);
+    rv(H);
+
+    vector<pair<ll, int>> st;
+
+    ll current_sum = 0;
+
+    vl ans;
+    ans.reserve(N);
+
+    for (ll val: H) {
+        int count = 1;
+
+        while (!st.empty() && st.back().fi < val) {
+            current_sum -= st.back().fi * st.back().se;
+            count += st.back().se;
+            st.pop_back();
+        }
+
+        st.pb(val, count);
+        current_sum += val * count;
+
+        ans.pb(current_sum + 1);
+    }
+
+    prv(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    if (Multitest) {
+        rd(T);
+    }
+    while (T--) {
+        solve();
+    }
+}
---
+++ leetcode/900-1000/968. 监控二叉树.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using ordered_set = tree<int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_map = tree<int, int, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset =
+    tree<pair<long long, long long>, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int N = int(5e5 + 5);
+int INIT = [] { return 0; }();
+
+using ll = long long;
+#define i128 __int128_t
+#define db long double
+#define pb emplace_back
+#define pf emplace_front
+#define pob pop_back
+#define ep emplace
+#define ins insert
+#define all(x) (x).begin(), (x).end()
+#define all2(x, i) (x).begin() + (i), (x).end()
+using pii = pair<ll, ll>;
+#define ull unsigned long long
+#define vi vector<int>
+#define vp vector<pii>
+#define vl vector<long long>
+#define vvi vector<vector<int>>
+#define vvp vector<vector<pii>>
+#define vvl vector<vector<long long>>
+#define D(i, j, k) for (int(i) = (j); (i) >= (k); (i)--)
+#define SZ(a) ((int) (a).size())
+#define prq priority_queue
+#define fi first
+#define se second
+constexpr int MOD = int(1e9 + 7);
+constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+constexpr int inf = 0x3f3f3f3f;
+#define F(i, j, k) for (int(i) = (j); (i) <= (k); (i)++)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    vi dfs(TreeNode *root) {
+        if (!root)
+            return {0, inf, 0};
+
+        auto L = dfs(root->left);
+        auto R = dfs(root->right);
+
+        vi res(3);
+
+        res[1] = 1 + min({L[0], L[1], L[2]}) + min({R[0], R[1], R[2]});
+
+        res[2] = min(L[0], L[1]) + min(R[0], R[1]);
+
+        int caseL = L[1] + min(R[0], R[1]);
+        int caseR = R[1] + min(L[0], L[1]);
+
+        res[0] = min(caseL, caseR);
+
+        return res;
+    }
+
+public:
+    int minCameraCover(TreeNode *root) {
+        auto res = dfs(root);
+        return min(res[0], res[1]);
+    }
+};
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     Solution sol;
+// }
